caec54fe46bdc6c0e8652b76709d890d24d45752
mostafaAli122 <mostafaali122@outlook.com>
2018-12-10 12:37:40 +0200

    Updating User Profile Image

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -8,8 +8,12 @@
                 <div class="panel-heading">Edit Your Profile.</div>
 
                 <div class="panel-body">
-                   <form action="{{ route('profile.update')}} " method="post">
+                   <form action="{{ route('profile.update')}} " method="post" enctype="multipart/form-data">
                         {{csrf_field()}}
+                        <div class="form-group">
+                            <label for="location">Upload Profile Image</label>
+                            <input type="file" name="avatar" class="form-control" accept="image/*">
+                        </div>
                         <div class="form-group">
                             <label for="location">location</label>
                             <input type="text" name="location" value="{{$info->location}} " class="form-control" required>
