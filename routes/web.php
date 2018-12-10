dea39d7084b0c6dd6e0f2114d77ac9f81959247d
mostafaAli122 <mostafaali122@outlook.com>
2018-12-10 10:57:55 +0200

    create user profile page

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +8,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/profile/{slug}',[
+        'uses'=> 'ProfilesController@index',
+        'as' => 'profile'
+    ]);
+});
