71c06cc99a854540e475475eb330e9d48673648b
mostafaAli122 <mostafaali122@outlook.com>
2018-12-11 15:52:35 +0200

    Completing friendable trait

--- app/Traits/Friendable.php
+++ app/Traits/Friendable.php
@@ -8,17 +8,27 @@ trait Friendable
    
     public function add_friend($user_requested_id){
 
+        if($this->id === $user_requested_id)
+            return 0;
+        if($this->is_friends_with($user_requested_id)===1)
+            return "already friends !!";
+        if($this->has_pending_friend_requests_sent_to($user_requested_id)===1)
+            return "Already sent a frriend request";
+        if($this->has_pending_friend_requests_from($user_requested_id)===1)
+            return $this->accept_friend($user_requested_id);        
         $Friendship=Friendship::create([
             'requester' => $this->id,
             'user_requested' => $user_requested_id
         ]);
         if($Friendship)
         {
-            return response()->json($Friendship,200);
+            return 1;
         }
-        return response()->json('fail',501);
+        return 0;
     }
-    public function accept_frienf($requester){
+    public function accept_friend($requester){
+        if($this->has_pending_friend_requests_from($requester)===0)
+            return 0;
         $friendship= Friendship::where('requester',$requester)
                                 ->where('user_requested',$this->id)
                                 ->first();
@@ -26,9 +36,9 @@ trait Friendable
            $friendship->update([
             'status' => 1
            ]);
-           return response()->json($friendship,200);
+           return 1;
         }
-        return response()->json('fail',501);
+        return 0;
     }
     public function friends(){
         $friends=array();
@@ -67,12 +77,51 @@ trait Friendable
 
     public function is_friends_with($user_id){
         if(in_array($user_id,$this->friends_ids())){
-            return response()->json('true',200);
+            return 1;
         }
         else{
-            return response()->json('false',200);
+            return 0;
         }
     }
+
+    public function pending_friend_requests_ids(){
+        return collect($this->pending_friend_requests())->pluck('id')->toArray();
+    }
+
+    public function pending_friend_requests_sent(){
+        $users=array();
+        $friendships=Friendship::where('status',0)
+                            ->where('requester',$this->id)
+                            ->get();
+        foreach($friendships as $friendship);
+            array_push($users,\App\Users::find($friendship->user_requested));
+        endforeach; 
+        return $users;               
+    }
+
+    public function pending_friend_requests_sent_ids(){
+        return collect($this->pending_friend_requests_sent())->pluck('id')->toArray();
+    }
+
+    public function has_pending_friend_requests_from($user_id){
+       if(in_array($user_id,$this->pending_friend_requests_ids())){
+           return 1;
+       }               
+       else{
+           return 0;
+       }
+    }
+    public function has_pending_friend_requests_sent_to($user_id){
+        if(in_array($user_id,$this->pending_friend_requests_ids())){
+            return 1;
+        }               
+        else{
+            return 0;
+        }
+     }
+
+
+
     
 
 }
