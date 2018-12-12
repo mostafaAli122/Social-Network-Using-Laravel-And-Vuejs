5716d8dd69819dd7d0afa860a07fa4770a20d0f5
mostafaAli122 <mostafaali122@outlook.com>
2018-12-12 12:42:37 +0200

    displaying friendship status

--- resources/assets/js/components/Friend.vue
+++ resources/assets/js/components/Friend.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="container">
         <div class="row">
-            Component Ready
+            <p class="text-center" v-if="loading">
+                Loading...
+            </p>
+            <p class="text-center" v-if="!loading">
+                <button class="btn btn-success" v-if="status==0">Add Friend</button>
+                <button class="btn btn-success" v-if="status=='pending'">Accept Friend</button>
+                <span class="text-center" v-if="status=='waiting'">Waiting For Response </span>
+                <span class="text-center" v-if="status=='friends'">Friends </span>
+                
+            </p>     
         </div>
     </div>
 </template>
@@ -12,8 +21,16 @@
             this.$http.get('/check_relationship_status/'+this.profile_user_id)
             .then((resp)=>{
                 console.log(resp)
+                this.status=resp.body.status
+                this.loading = false
             })
         },
         props:['profile_user_id'],
+        data(){
+            return {
+                status: '',
+                loading: true
+            }
+        }
     }
 </script>
