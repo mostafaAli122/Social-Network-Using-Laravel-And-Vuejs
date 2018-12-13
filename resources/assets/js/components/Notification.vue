b41ba8602052991f15d2c602848734c81929b900
mostafaAli122 <mostafaali122@outlook.com>
2018-12-13 14:06:21 +0200

    listening to events using laravel echo

---
+++ resources/assets/js/components/Notification.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    export default{
+        ready() {
+            this.listen()
+        },
+        props:['id'],
+        methods:{
+            listen(){
+                Echo.private('App.User'+this.id)
+                    .notification((notification)=>{
+                        alert('new notification')
+                        console.log(notification)
+                    })
+            }
+        }
+    }
+</script>
