9b712032f399b3315a43f079e63fba5822ae6b87
mostafaAli122 <mostafaali122@outlook.com>
2018-12-16 10:06:43 +0200

    save posts to database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function store(Request $request){
+        return Post::create([
+            'user_id' => Auth::id(),
+            'content' => $request->content    
+        ]);
+    }
 }
