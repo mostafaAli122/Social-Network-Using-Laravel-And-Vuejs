08b043f19ee0096f6dbaefe66a9ec65664eb9332
mostafaAli122 <mostafaali122@outlook.com>
2018-12-17 10:25:14 +0200

    installation of laravel scout and algolia driver

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 use Storage;
 use App\Traits\Friendable;
+use Laravel\Scout\Searchable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -10,6 +11,7 @@ class User extends Authenticatable
 {
     use Notifiable;
     use Friendable;
+    use Searchable;
 
     /**
      * The attributes that are mass assignable.
